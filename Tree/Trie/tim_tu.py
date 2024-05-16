6749790895c8a8ea15d838017cf3f73c3b60a342
thailehoangbao <thaibao29111994@gmail.com>
2024-05-16 21:27:14 +0700

    demo tim kiem

--- Tree/Trie/tim_tu.py
+++ Tree/Trie/tim_tu.py
@@ -1,22 +1,26 @@
 # Danh sách từ
-arrChar = ["car", "bat", "card", "man", "women", "bag"]
+arrChar = ["cat", "banana", "obama", "batman", "car", "cow","alibaba"]
 
 # Nhập từ muốn kiếm
 getChar = input("Nhập từ muốn kiếm: ")
 
 # Danh sách kết quả
 results = []
+vitri = []
 
+i = 0
 # Duyệt qua từng phần tử trong danh sách arrChar
 for word in arrChar:
     # Kiểm tra nếu từ bắt đầu bằng chuỗi ký tự nhập vào
     if word.startswith(getChar):
         # Thêm từ vào danh sách kết quả
         results.append(word)
+        vitri.append(i)
+    i+=1
 
 # In kết quả
-print("Kết quả:", results)
-
+print("Kết quả: ", results)
+print("Vị trí: ", vitri)
 #Trie Cấu trúc
 root = {
     "c": {
