df890dac86190576193f0af2e56f0b7d16679992
thailehoangbao <thaibao29111994@gmail.com>
2024-06-07 10:49:59 +0700

    bigO bt 4

---
+++ BT_BigO/BigO_bt4.py
@@ -0,0 +1,10 @@
+a = 1
+b = 2
+while (a < n):          
+    while (b < n):      
+        a = a * 2       
+        b = b * 2       
+
+# F(N) = 1 + logN + logN^2 + 2*logN^2
+# Độ phức tạp thời gian chạy của đoạn code trên là O(logN^2)
+
